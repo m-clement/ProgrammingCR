4738a6ba8b8c207b2def16b19d4faf2195401f93
Pierre Clement <Pierre.Clement@ithaka.org>
2017-05-29 18:50:51 -0400

    moving on to next objective

--- Objective6/calculator/mysite/mysite/urls.py
+++ Objective6/calculator/mysite/mysite/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^calculator/', include('calculator.urls')),
+    url(r'^polls/', include('polls.urls')),
 	url(r'^admin/', admin.site.urls),
 ]
