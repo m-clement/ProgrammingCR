b4d31bc9c73f23e3d754d5d09b3158b094e8580e
Pierre Clement <Pierre.Clement@ithaka.org>
2017-05-31 19:19:26 -0400

    first pass after generating structure

---
+++ Objective7/README.md
@@ -0,0 +1,12 @@
+# Objective 7: Complete Web Site Development
+
+References 
+- https://start.spring.io/
+- 
+
+## Various things we've learned
+-
+
+## Assignement findings
+
+* 
---
+++ Objective7/texteditor/src/test/java/org/miguel/texteditor/TexteditorApplicationTests.java
@@ -0,0 +1,16 @@
+package org.miguel.texteditor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TexteditorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
